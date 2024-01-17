75d0fca2fc1463aa0c0ecf26feb299cdbe6e7364
Admin <artem-ruslanovich@inbox.ru>
2024-01-17 12:32:17 +0300

    Hello world

--- src/main/java/com/discord/bot/Bot.java
+++ src/main/java/com/discord/bot/Bot.java
@@ -5,7 +5,7 @@ import net.dv8tion.jda.api.JDABuilder;
 public class Bot {
     public static void main(String[] args) throws Exception {
         JDABuilder.createDefault("YOUR_BOT_TOKEN_HERE")
-            .addEventListeners(new MyListener()) // Замените MyListener на ваш собственный класс для обработки событий
+            .addEventListeners(new MyListener()) 
             .build();
     }
 }
