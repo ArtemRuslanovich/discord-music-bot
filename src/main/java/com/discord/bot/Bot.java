60e42fd0640362d7378c07c2264e08b36f3b0c72
Artiom Zavatski <artiomzavatski@gmail.com>
2024-02-18 12:33:27 +0300

    token

--- src/main/java/com/discord/bot/Bot.java
+++ src/main/java/com/discord/bot/Bot.java
@@ -5,7 +5,7 @@ import net.dv8tion.jda.api.requests.GatewayIntent;
 
 public class Bot {
     public static void main(String[] args) throws Exception {
-        JDABuilder.createDefault("x")
+        JDABuilder.createDefault("y")
             .addEventListeners(new MusicBot())
             .enableIntents(GatewayIntent.MESSAGE_CONTENT) 
             .build();
